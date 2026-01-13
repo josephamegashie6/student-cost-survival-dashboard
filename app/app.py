786b0561398b4656d9745ccbf152802d78d4b4d9
josephamegashie6 <josephligi6@gmail.com>
2026-01-13 07:44:13 -0600

    Add income and expenses calculations

--- app/app.py
+++ app/app.py
@@ -1,6 +1,22 @@
 import pandas as pd
 import streamlit as st
 st.title("International Student Cost Survival Dashboard")
-data = pd.read_csv("data/student_Costs.csv")
+data = pd.read_csv("data/student_costs.csv")
 st.subheader("Monthly Cost Data")
 st.dataframe(data)
+
+#calculation
+data["total_income"] = data["campus_job_income"] + data["stipend_income"]
+expense_columns = [
+  "rent",
+  "utilities",
+  "food",
+  "transport",
+  "phone_internet",
+  "misc_basic"]
+
+data["total_expenses"] = data[expense_columns).sum(axis=1)
+data["balance"] = data["total_income"] - data["total_expenses']
+
+st.subheader("Financial_Summary")
+st.dataframe(data[["city", "month", "total_income", "total_expenses", "balance"]])
